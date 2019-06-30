525f93073b8914aa03a6f9b8d102f0732e7d196b
Conor Brady <conorpbrady@gmail.com>
2019-06-30 11:21:01 -0400

    add init pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,25 @@
 class Board
 
   def initialize
-
+    white_pieces = []
+    black_pieces = []
   end
 
   def draw
 
   end
+
+  def init_pieces
+    colors = ['W','B']
+      white_pieces.add(Rook.new('A1'))
+      white_pieces.add(Knight.new('B1'))
+      white_pieces.add(Bishop.new('C1'))
+      white_pieces.add(Queen.new('D1'))
+      white_pieces.add(King.new('E1'))
+      white_pieces.add(Bishop.new('F1'))
+      white_pieces.add(Knight.new('G1'))
+      white_pieces.add(Rook.new('H1'))
+
+    end
 
 end
