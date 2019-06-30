12960d4b6236d84e504b4070b394b89188b1d7b3
Conor Brady <conorpbrady@gmail.com>
2019-06-30 11:09:02 -0400

    add moves to GamePiece

--- lib/game_piece.rb
+++ lib/game_piece.rb
@@ -1,5 +1,5 @@
 class GamePiece
-  attr_accessor :position
+  attr_accessor :position, :symbol, :color
 
   def initialize(position)
     @position = position
@@ -9,5 +9,53 @@ end
 
 
 class King > GamePiece
-
+  @symbol = 'K'
+  @moves = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]
+end
+class Queen > GamePiece
+  @symbol = 'Q'
+  @moves = [
+    [0,'x'],
+    ['x','-x'],
+    ['x',0],
+    ['x','x'],
+    [0,'x'],
+    ['-x','x'],
+    ['-x',0],
+    ['-x','-x']
+    ]
+end
+class Bishop > GamePiece
+  @symbol = 'B'
+  @moves =  [
+    ['x','x'],
+    ['-x','x'],
+    ['x','-x'],
+    ['-x','-x']
+  ]
+end
+class Knight > GamePiece
+  @symbol = 'N'
+  @moves = [
+    [1,2],
+    [2,1],
+    [-1,2],
+    [-2,1],
+    [1,-2],
+    [2,-1],
+    [-1,-2],
+    [-2,-1]
+  ]
+end
+class Rook > GamePiece
+  @symbol = 'R'
+  @moves = [
+    [0,'x'],
+    [0,'-x'],
+    ['x',0],
+    ['-x',0]
+  ]
+end
+class Pawn > GamePiece
+  @symbol = 'P'
 end
