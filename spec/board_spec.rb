66fe0c85df65062ba0f7a609a179a0ac1ccf7785
Conor Brady <conorpbrady@gmail.com>
2019-07-03 18:15:03 -0400

    rename board testing to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,10 @@
-#require './lib/board.rb'
+require './lib/board.rb'
 
 RSpec.describe Board do
   describe "#init_pieces" do
     it "intializes game pieces" do
       b = Board.new
-      b.init_pieces
-      expect(b.to eql(14))
+      expect(b.init_pieces).to eql(14)
     end
   end
 end
